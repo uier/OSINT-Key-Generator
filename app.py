f15473152b141b5e082b04cdf54f8c760201a7f6
Uier <ap9940506@gmail.com>
2020-07-31 20:41:30 +0800

    gino good

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import tempfile
+import time
 from flask import Flask, render_template, request, send_file
 
 app = Flask(__name__, template_folder='./templates')
@@ -10,10 +11,10 @@ def index():
 @app.route('/osint', methods=['GET', 'POST'])
 def osint():
     if request.method == 'GET':
-        return render_template('osint.html', osint_result='GET')
+        return render_template('osint.html', osint_result='GET', data={})
     elif request.method == 'POST':
-        print(request)
         if request.form['action'] == 'GENERATE':
+            time.sleep(5)
             txt = tempfile.TemporaryFile()
             txt.write(b'123')
             txt.seek(0)
